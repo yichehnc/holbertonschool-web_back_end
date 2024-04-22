311476344b67240cef2ab25eee015003439a66de
yichehnc <yichenc2017@gmail.com>
2024-04-23 09:45:34 +1000

    feat: Write a type-annotated function add that takes a float a and float b as arguments and returns their sum as a float

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function add that takes a float a and a float b as arguments
+"""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floats"""
+    return a + b
