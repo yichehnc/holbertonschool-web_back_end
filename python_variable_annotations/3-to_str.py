9ce2544a2dbcda0fc91d0ef49ec1d3e7511e94a8
yichehnc <yichenc2017@gmail.com>
2024-04-26 06:28:24 +1000

    feat: a type-annotated function to_str that takes a float n as argument and returns the string representation of the flaot

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function to_str that takes a float n as argument and returns
+the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float n as argument and returns the string representation of the float
+    """
+    return str(n)
