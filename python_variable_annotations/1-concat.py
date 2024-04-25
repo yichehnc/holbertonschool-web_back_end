1faba1ae00fbfba68f66c28f5cdbafba70f58ec6
yichehnc <yichenc2017@gmail.com>
2024-04-26 08:30:33 +1000

    style: correct indentation

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@ A type-annotated function concat that takes a string str1 and a string str2 as
 arguements and returns a concatenated string
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """Return the concatenation of two strings"""
     return str1 + str2
