7dd824e849bc85305872e07921f75b5153290e80
yichehnc <yichenc2017@gmail.com>
2024-04-26 08:30:19 +1000

    style: correct indentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,6 +3,7 @@
 A type-annotated function add that takes a float a and a float b as arguments
 """
 
+
 def add(a: float, b: float) -> float:
     """Return the sum of two floats"""
     return a + b
