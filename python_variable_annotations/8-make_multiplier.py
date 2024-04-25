d4df0d3fbd18fea3098245f6b21b1e3d29f72a00
yichehnc <yichenc2017@gmail.com>
2024-04-26 08:05:51 +1000

    feat: a type-annotated function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -11,4 +11,6 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     Takes a float multiplier as argument and returns a function that multiplies
     a float by multiplier
     """
-    return lambda x: x * multiplier
+    def multiply(n: float) -> float:
+        return n * multiplier
+    return multiply
