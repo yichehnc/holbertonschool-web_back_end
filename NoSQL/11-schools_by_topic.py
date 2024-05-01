9ab94f4b8c72672003cc2882855325fb6834da51
yichehnc <yichenc2017@gmail.com>
2024-05-02 08:55:22 +1000

    feat: a function that returns the list of school having a specific topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A function that returns the list of school having a specific topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic
+    """
+    return mongo_collection.find({"topics": topic})
