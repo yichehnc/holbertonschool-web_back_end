4f188cf9a5e7b89fe36215c0b3b675180f0ab5be
yichehnc <yichenc2017@gmail.com>
2024-05-17 18:00:13 +1000

    feat: Returns promises and log body firstName lastName to the console

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  return Promise.all([promise1, promise2])
+    .then((values) => {
+      console.log(
+        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`,
+      );
+    })
+    .catch(() => console.log('Signup system offline'));
+}
