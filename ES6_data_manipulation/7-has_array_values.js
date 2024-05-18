5927248978bb4cd217f67117bb4c1de82daf3e14
yichehnc <yichenc2017@gmail.com>
2024-05-19 00:00:38 +1000

    feat: Create a function that returns a boolean if all the elements in the array exist within the set

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,8 @@
+xport default function hasValuesFromArray(set, array) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (!set.has(array[i])) {
+      return false;
+    }
+  }
+  return true;
+}
