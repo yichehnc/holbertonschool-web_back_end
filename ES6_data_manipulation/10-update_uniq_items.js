2f25af94156b2beaa727c46786f37931b05666f0
yichehnc <yichenc2017@gmail.com>
2024-05-20 11:00:44 +1000

    feat: Create a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(items) {
+  if (!(items instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of items) {
+    if (value === 1) {
+      items.set(key, 100);
+    }
+  }
+
+  return items;
+}
